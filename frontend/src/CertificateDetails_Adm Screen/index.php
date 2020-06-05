b0ebefb12e07cd2bac123ec3904ca0adde9bfeb0
Lademir Junior <lademir.junior@ufv.br>
2020-06-05 15:42:00 -0300

    comeco da tela de cadastro

--- frontend/src/CertificateDetails_Adm Screen/index.php
+++ frontend/src/CertificateDetails_Adm Screen/index.php
@@ -5,7 +5,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Detalhes do Certificados do Usuário</title>
+    <title>Detalhes do Certificados dos Usuários</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -43,7 +43,9 @@
             <i class="right angle icon divider"></i>
             <a class="section" href="../AdmPage/">Gerenciador de Certificados</a>
             <i class="right angle icon divider"></i>
-            <div class="active section">Detalhes do Certificados</div>
+            <a class="section" href="../CertificatesValidation/">Validação de Certificados</a>
+            <i class="right angle icon divider"></i>
+            <div class="active section">Detalhes dos Certificados dos Usuários</div>
         </div>
 
         <div class="ui divider"></div>
@@ -75,7 +77,8 @@
             <button class="ui button blue" onclick="handleCertificateDownload()">Baixar PDF</button>
             <button class="ui button blue" onclick="navigateToEditCertificate()">Editar dados</button> 
         </div>
-
+ 
+ 
     </div>
 </body>
 </html>
